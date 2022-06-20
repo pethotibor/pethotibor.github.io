e4859278d7ff77a3e8b2a4cfd77f18a4a3be4ecb
pethotibor <petho.tibor@gmail.com>
2022-06-21 01:49:17 +0200

    htmls done

---
+++ src/main/java/hu/nye/algoritmusok_tervezese/Itinerary_alternatives/ItineraryAlternativesApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.algoritmusok_tervezese.Itinerary_alternatives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItineraryAlternativesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItineraryAlternativesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/nye/algoritmusok_tervezese/Itinerary_alternatives/ItineraryAlternativesApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.algoritmusok_tervezese.Itinerary_alternatives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItineraryAlternativesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
